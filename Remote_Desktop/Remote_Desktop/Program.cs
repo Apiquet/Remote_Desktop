d8cca7384b4e0dd5d75e038a1e57fe2447ffac17
Anthony Piquet <anthony.piquet2@gmail.com>
2018-05-26 20:13:50 -0700

    Function to open a program on terminal session

--- Remote_Desktop/Remote_Desktop/Program.cs
+++ Remote_Desktop/Remote_Desktop/Program.cs
@@ -11,29 +11,32 @@ namespace Remote_Desktop
     {
         static void Main(string[] args)
         {
+            var prog = new Program();
             try
             {
                 var connection = new ConnectionOptions();
                 connection.Username = "";
                 connection.Password = "";
-
                 var wmiScope = new ManagementScope(String.Format("\\\\{0}\\root\\cimv2", "IP_Address"), connection);
-                
                 var wmiProcess = new ManagementClass(wmiScope, new ManagementPath("Win32_Process"), new ObjectGetOptions());
-
-                //to open a program on the terminal session we need to schedule a task, then run it 
-                //task creation
-                object[] methodArgs = { "SchTasks /Create /SC DAILY /TN MyTest /TR notepad /ST 09:00", null, null, 0 };
-                object result = wmiProcess.InvokeMethod("Create", methodArgs);
-                //task runner
-                methodArgs[0] = "schtasks /Run /TN MyTest";
-                result = wmiProcess.InvokeMethod("Create", methodArgs);
+                prog.OpenProgramOnTerminalSession(wmiProcess, "notepad");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 throw;
             }
+        }
+
+        public void OpenProgramOnTerminalSession(ManagementClass wmiProcess, string command)
+        {
+            //to open a program on the terminal session we need to schedule a task, then run it 
+            //task creation
+            object[] methodArgs = { "SchTasks /Create /SC DAILY /TN MyTest /TR " + command + " /ST 09:00", null, null, 0 };
+            object result = wmiProcess.InvokeMethod("Create", methodArgs);
+            //task runner
+            methodArgs[0] = "schtasks /Run /TN MyTest";
+            result = wmiProcess.InvokeMethod("Create", methodArgs);
         }
     }
 }
