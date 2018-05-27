6c3caa9d31625249dc2a2ebfd0f3e395e6d186a4
Anthony Piquet <anthony.piquet2@gmail.com>
2018-05-26 19:40:25 -0700

    Running notepad in background on remote computer

--- Remote_Desktop/Remote_Desktop/Program.cs
+++ Remote_Desktop/Remote_Desktop/Program.cs
@@ -16,6 +16,15 @@ namespace Remote_Desktop
                 var connection = new ConnectionOptions();
                 connection.Username = "";
                 connection.Password = "";
+
+                var wmiScope = new ManagementScope(String.Format("\\\\{0}\\root\\cimv2", "IP_Address"), connection);
+                //wmiScope.Connect();
+                var wmiProcess = new ManagementClass(wmiScope, new ManagementPath("Win32_Process"), new ObjectGetOptions());
+                // arguments for the method  
+                object[] methodArgs = { "notepad", null, null, 0 };
+
+                //Execute the method 
+                object result = wmiProcess.InvokeMethod("Create", methodArgs);
             }
             catch (Exception e)
             {
